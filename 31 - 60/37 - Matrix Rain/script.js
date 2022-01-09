8cf82af0f3caa63eaefb1d7a7fc67819c3bc96ad
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-09 11:05:38 -0300

    randow delay between columns

--- 31 - 60/37 - Matrix Rain/script.js
+++ 31 - 60/37 - Matrix Rain/script.js
@@ -16,7 +16,7 @@ class Symbol{
         this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
         context.fillStyle = '#0aff0a'
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if(this.y * this.fontSize > this.canvasHeight){
+        if(this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
             this.y = 0;
         }else{
             this.y += 1;
