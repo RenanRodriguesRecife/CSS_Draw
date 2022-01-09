c49863d94da2a617207786a5514cc00e972ab549
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-09 10:15:57 -0300

    character move down

--- 31 - 60/37 - Matrix Rain/script.js
+++ 31 - 60/37 - Matrix Rain/script.js
@@ -14,7 +14,13 @@ class Symbol{
     }
     draw(context){
         this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
-        
+        context.fillStyle = '#0aff0a'
+        context.fillStyle(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if(this.y * this.fontSize > this.canvasHeight){
+            this.y = 0;
+        }else{
+            this.y += 1;
+        }
     }
 }
 
