4f6510c104bda7288041cec98a8aae5ef01e469a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-09 11:01:56 -0300

    make old symbols fade away

--- 31 - 60/37 - Matrix Rain/script.js
+++ 31 - 60/37 - Matrix Rain/script.js
@@ -15,7 +15,7 @@ class Symbol{
     draw(context){
         this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
         context.fillStyle = '#0aff0a'
-        context.fillStyle(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
         if(this.y * this.fontSize > this.canvasHeight){
             this.y = 0;
         }else{
@@ -32,14 +32,23 @@ class Effect{
         this.fontSize = 25;
         this.columns = this.canvasWidth/this.fontSize;
         this.symbols = []
+        this.#initialize();
+        console.log(this.symbols)
     }
     #initialize(){
-        for(let i = 0; i < this.columns.length; i++){
-            this.symbols[i] = new Symbol();
+        for(let i = 0; i < this.columns; i++){
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
         }
     }
 }
 
-function animate(){
+const effect = new Effect(canvas.width, canvas.height);
 
+function animate(){
+    ctx.fillStyle = 'rgba(0,0,0,0.05)'
+    ctx.fillRect(0,0,canvas.width, canvas.height)
+    ctx.font = effect.fontSize + 'px monospace';
+    effect.symbols.forEach(symbol => symbol.draw(ctx))
+    requestAnimationFrame(animate);
 }
+animate();
