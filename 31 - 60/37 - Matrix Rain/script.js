54d4914cceebfd7b439c9cdb1a278946cbce20f7
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-01-09 10:05:14 -0300

    symbol constructor

--- 31 - 60/37 - Matrix Rain/script.js
+++ 31 - 60/37 - Matrix Rain/script.js
@@ -2,3 +2,17 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+class Symbol{
+    constructor(x, y, fontSize, canvasHeight){
+        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight = canvasHeight;
+    }
+    draw(){
+
+    }
+}
